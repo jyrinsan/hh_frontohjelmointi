0a09f8bfcb13863cb0f58f776f10a3091c727911
Sanna Jyrinki <AA8206@student.jamk.fi>
2021-11-21 17:21:59 +0200

    buttontheme

---
+++ buttontheme/src/HeaderButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ThemeContext from './ThemeContext';
+
+function HeaderButton() {
+  const themeContext = React.useContext(ThemeContext);
+
+  console.log('themeContext', themeContext.backgroundColor)
+
+  return (
+    <div>
+      <button style={{color: themeContext.color, backgroundColor: themeContext.backgroundColor}}>Press me</button>
+    </div>
+  );
+}
+
+export default HeaderButton;
